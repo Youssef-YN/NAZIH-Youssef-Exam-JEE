9c6b99dab5c6a8b8c8f59e858b5c3c3720f6cd3c
Youssef-YN <nazihyoussef05@gmail.com>
2026-05-11 08:45:36 +0100

    setup backend

---
+++ backend/src/test/java/nazih/youssef/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nazih.youssef.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
